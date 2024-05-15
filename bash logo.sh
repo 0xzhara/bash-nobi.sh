168b0019a3f38e8e48934d2073a07d1191d662a4
zrhra.eth [Jettokōsutā] <122523974+zrhraJETTOKOSUTA@users.noreply.github.com>
2024-05-15 11:37:04 +0700

    Create bash logo.sh

---
+++ bash logo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Displaying Airdrop Nobi logo..."
+sleep 1
+
+cat << "EOF"
+   _   _ _           _                       _   _        _ _ 
+  /_\ | | | ___  ___| |_    __ _ _   _  __ _| |_| |_ _ __(_) |
+ //_\\| | |/ _ \/ __| __|  / _` | | | |/ _` | __| __| '__| | |
+/  _  \ | |  __/\__ \ |_  | (_| | |_| | (_| | |_| |_| |  | | |
+\_/ \_/_|_|\___||___/\__|  \__, |\__,_|\__,_|\__|\__|_|  |_|_|
+                            |___/                            
+EOF
+
+echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
+sleep 2
